39775a34e7bfbe81f47814609ad13fe054ad9162
Ryan LIU <ryanlyy@126.com>
2018-12-07 15:27:15 +0800

    Update tkill.c

--- tkill.c
+++ tkill.c
@@ -1,16 +1,32 @@
-#include <sys/syscall.h>
-#include <sys/types.h>
-#include <unistd.h>
+#define _GNU_SOURCE
+
 #include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
 
-int main(int argc, char** argv)
+#define TGKILL 270
+
+
+int main(int argc, char *argv[])
 {
-        int tg_id = atoi(argv[1]);
-        int t_id = atoi(argv[2]);
-        int sig = atoi(argv[3]);
-        int ret;
+    long ret;
+    int tgid, tid;
+    int sig;
 
-        printf("sending signal:%d to thread:%d of process:%d...\n", t_id, tg_id);
-        return syscall(SYS_tgkill, tg_id, t_id, sig);
+    tgid = atoi(argv[1]);
+    tid = atoi(argv[2]);
+    sig = atoi(argv[3]);
+
+
+    //tid = syscall(SYS_gettid);
+    //ret = syscall(SYS_tgkill, tgid, tid, SIGTERM);
+    ret = syscall(SYS_tgkill, tgid, tid, sig);
+
+    if (ret != 0)
+        perror("tgkill");
+
+    return 0;
 }
